26fc6cbef794dc9b38554be8817a624daade0a63
lilott8 <lilott8@gmail.com>
2020-03-25 17:15:06 +0000

    This is now able to handle systemd control

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import signal
 import colorlog
 import logging
 import board
@@ -13,34 +14,49 @@ from sensors.sgp30 import SGP30
 from misc.db import DB
 from misc.units import Temp
 import os
+from typing import Dict
 
 
-def main(args):
-    with open(args.config) as c:
-        config = json.load(c)
-    db = DB(config['database'])
-    
-    temperature = Temp.get_from_string(config['temperature'])
-    i2c = busio.I2C(board.SCL, board.SDA)
-    
-    sensors = [
-            BME680(i2c, temperature, config['pressure'], config['location']), 
-            SGP30(i2c, temperature, config['location'])
-            ]
-    logging.info("Done initializing sensors")
+class Environment(object):
 
-    while True:
-        response = list()
-        for sensor in sensors:
-            sensor.calibrate(5)
-            if config['debug']:
-                sensor.debug()
-            response.extend(sensor.read())
-        if config['debug']:
-            logging.warn(response)
-        db.write(response)
-        time.sleep(120)
-    db.close()
+    def __init__(self, config: str):
+        self.logger = colorlog.getLogger(self.__class__.__name__)
+        signal.signal(signal.SIGINT, self.shutdown)
+        signal.signal(signal.SIGTERM, self.shutdown)
+        with open(args.config) as c:
+            self.config = json.load(c)
+        self.db = DB(self.config['database'])
+        self.temperature = Temp.get_from_string(self.config['temperature'])
+
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensors = [
+                BME680(i2c, self.temperature, self.config['pressure'], self.config['location']), 
+                SGP30(i2c, self.temperature, self.config['location'])
+                ]
+        self.alive = True
+        pass
+
+    def run(self):
+        for sensor in self.sensors:
+            sensor.calibrate(2)
+        logging.info("Done initializing sensors")
+
+        while self.alive:
+            response = list()
+            for sensor in self.sensors:
+                sensor.calibrate(5)
+                if self.config['debug']:
+                    sensor.debug()
+                response.extend(sensor.read())
+            if self.config['debug']:
+                self.logger.warn(response)
+            self.db.write(response)
+            time.sleep(120)
+        self.db.close()
+
+    def shutdown(self, signum, frame):
+        self.logger.warn("Shutting down environment")
+        self.alive = False
 
 if __name__ == "__main__":
     colorlog.basicConfig(level=logging.DEBUG,
@@ -50,4 +66,5 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--config', help="Path to the config", default=default_path)
     args = parser.parse_args(sys.argv[1:])
 
-    main(args)
+    environment = Environment(args)
+    environment.run()
