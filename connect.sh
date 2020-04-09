88760a4c2f4d46bd2d49cfb818fa6246ea90c2c6
lilott8 <lilott8@gmail.com>
2020-04-09 16:30:37 +0100

    Added connection script so I don't have to remember it

---
+++ connect.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+	echo "Usage: ./connect {user} {database} [password]"
+	exit 0
+	fi
+
+PASSWORD=""
+USER=$1
+DATABASE=$2
+
+if [ $# -eq 3 ]
+then
+	PASSWORD="-password $3"
+	fi
+
+COMMAND="influx -username $USER $PASSWORD -database $DATABASE"
+echo $COMMAND
