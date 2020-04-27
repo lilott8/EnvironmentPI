5fe7afb214befa367ce8f7e261add4da9bbd0069
lilott8 <lilott8@gmail.com>
2020-04-27 06:15:42 +0100

    Added shims for getting loggers

---
+++ misc/logging.py
@@ -0,0 +1,13 @@
+import sys
+import logging
+import logging.handlers
+import colorlog
+
+
+# https://github.com/borntyping/python-colorlog/issues/43
+def get_syslog():
+    pass
+
+
+def get_stdout_log():
+    pass
